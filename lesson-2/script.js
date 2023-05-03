b09898eeeea3a2975090adf65a6703c8b1767889
R4dche <radchenko.g97@gmail.com>
2023-05-03 20:37:29 +0300

    fixed a code styling

--- lesson-2/script.js
+++ lesson-2/script.js
@@ -1,6 +1,7 @@
 function calc() {
     const firstNumber = +prompt('Enter first number');
     const secondNumber = +prompt('Enter second numer');
+
     alert(
         `${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}
 ${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}
@@ -9,3 +10,4 @@ ${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
 }
 
 calc()
+
