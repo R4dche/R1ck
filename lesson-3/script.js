b7e1297aaf2982243bc6ba60024ce9d22dc01def
R4dche <radchenko.g97@gmail.com>
2023-05-09 12:33:02 +0300

    added function userPortait

--- lesson-3/script.js
+++ lesson-3/script.js
@@ -1,39 +1,55 @@
-let dateUser = +prompt('Enter please your birth date');
-let currentDate = new Date().getFullYear - dateUser;
-// let cityUser = prompt('Enter please your native city').toLowerCase();
-let sportUser  = prompt('Enter please your favorite sport');
-let otherCity;
-let Ukraine;
-let USA;
-let greatBritain;
+function userPortrait () {
 
-switch (cityUser) {
-    case 'kiev':
-        Ukraine = alert('You live in the capital of Ukraine');
-        break;
-    case 'washington':
-        USA = alert('You live in the capital of USA');
-        break;
-    case 'london':
-        greatBritain = alert('You live in the capital of Greate Britain');
-        break;
-    default:    
-        otherCity = alert(`You live in the ${cityUser.toUpperCase()}`);
+    let dateUser = +prompt('Enter please your birth date');
+    let currentDate = new Date().getFullYear() - dateUser;
+    let cityUser = prompt('Enter please your native city').toLowerCase();
+    let sportUser  = prompt('Enter please your favorite sport');
+
+
+    let otherCity;
+    let Ukraine;
+    let USA;
+    let greatBritain;
+    
+    switch (cityUser) {
+        case 'kiev':
+            Ukraine = ('You live in the capital of Ukraine');
+            break;
+        case 'washington':
+            USA = ('You live in the capital of USA');
+            break;
+        case 'london':
+            greatBritain = ('You live in the capital of Greate Britain');
+            break;
+        default:    
+            otherCity = (`You live in the ${cityUser.toUpperCase()}`);
+    }
+    
+    let otherSport;
+    let basketball;
+    let box;
+    let football;
+    
+    switch (sportUser) {
+        case 'basketball':
+            basketball = ('Cool! Do you want to be Kobe Bryant?');
+            break;
+        case 'box':
+            box = ('Cool! Do you want to be Mike Tyson?');
+            break;
+        case 'football':
+            football = ('Cool! Do you want to be Cristiano Ronaldo?');
+            break;
+        default:
+            otherSport = (`Cool! Do you like ${sportUser}`);
+    }
+
+    return alert(`
+        You are ${currentDate} years old;
+        ${USA || Ukraine || greatBritain || otherCity};
+        ${basketball || box || football || otherSport};
+        `)
 }
 
-let otherSport;
-let favoriteSport;
+userPortrait();
 
-switch (sportUser) {
-    case 'basketball':
-        favoriteSport = alert('Cool! Do you want to be Kobe Bryant?');
-        break;
-    case 'box':
-        favoriteSport = alert('Cool! Do you want to be Mike Tyson?');
-        break;
-    case 'football':
-        favoriteSport = alert('Cool! Do you want to be Cristiano Ronaldo?');
-        break;
-    default:
-        otherSport = alert('')
-}
