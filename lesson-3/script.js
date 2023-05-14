852324bc237475ca0ebca9a86bd76eac87620ad5
R4dche <radchenko.g97@gmail.com>
2023-05-14 21:16:09 +0300

    fix a bug

--- lesson-3/script.js
+++ lesson-3/script.js
@@ -1,89 +1,95 @@
-function userPortrait () {
+function userPortrait() {
+  let dateUser;
+  let dateUserIsNumber;
 
-    let dateUser;
-    
-    function trueDate () {
-        return !dateUser || dateUser < 1900 || dateUser > 2005;
+  function validateUserBirthDate() {
+    return !dateUser || dateUser < 1900 || dateUser > 2005 || isNaN(dateUserIsNumber);  
+  }
+
+  function validateUserCitySport (validate) {
+    if (!validate || validate == ' ') {
+      return true;
     }
+  }
 
-    do {
-        dateUser = prompt('Enter please your birth date');
+  do {
+    dateUser = prompt("Enter please your birth date");
+    dateUserIsNumber = Number(dateUser);
 
-        if (trueDate(dateUser)) {
-            alert('It is a pity that you did not want to enter your birth date...');
-        }
-    } while (trueDate(dateUser));
-
-
-    let currentDate = new Date().getFullYear() - dateUser;
-
-    let cityUser;
-
-    function trueCity () {
-        return !cityUser || cityUser == ' ';
+    if (validateUserBirthDate(dateUser)) {
+      alert("It is a pity that you did not want to enter your birth date...");
     }
+  } while (validateUserBirthDate(dateUser));
 
-    do {
-        cityUser = prompt('What city do you live in?');
+  let userAge = new Date().getFullYear() - dateUser;
 
-        if (trueCity(cityUser)) {
-            alert('It is a pity that you did not want to enter your city...');
-        }
-    } while (trueCity(cityUser));
+  let userCity;
+  let otherCity;
+  let capitalCity;
 
-    let toLowerCaseCity = cityUser.toLowerCase();
-    let otherCity;
-    let capitalCity;
-    
-    switch (toLowerCaseCity) {
-        case 'kiev','kyiv':
-            capitalCity = ('You live in the capital of Ukraine');
-            break;
-        case 'washington':
-            capitalCity = ('You live in the capital of USA');
-            break;
-        case 'london':
-            capitalCity = ('You live in the capital of Greate Britain');
-            break;
-        default:    
-            otherCity = (`You live in the ${cityUser}`);
+  do {
+    userCity = prompt("What city do you live in?");
+
+    if (validateUserCitySport(userCity)) {
+      alert("It is a pity that you did not want to enter your city...");
     }
-    
-    let sportUser;
-    let otherSport;
-    let favoriteSport;
+  } while (validateUserCitySport(userCity));
 
-    function trueSport () {
-        return !sportUser || sportUser == ' ';
+  let toLowerCaseCity = userCity.toLowerCase();
+
+  switch (toLowerCaseCity) {
+    case "kiev":
+      capitalCity = "You live in the capital of Ukraine";
+      break;
+    case 'kyiv':
+        capitalCity = "You live in the capital of Ukraine";
+        break;
+    case "washington":
+      capitalCity = "You live in the capital of USA";
+      break;
+    case "london":
+      capitalCity = "You live in the capital of Greate Britain";
+      break;
+    default:
+      otherCity = `You live in the ${userCity}`;
+  }
+
+  let userSport;
+  let otherSport;
+  let favoriteSport;
+
+  do {
+    userSport = prompt("Enter please your favorite sport");
+
+    if (validateUserCitySport(userSport)) {
+      alert("It is a pity that you did not want to enter your sport...");
     }
+  } while (validateUserCitySport(userSport));
 
-    do {
-        sportUser = prompt('Enter please your favorite sport');
+  let toLowerCaseSport = userSport.toLowerCase();
 
-        if (trueSport(sportUser)) {
-            alert('It is a pity that you did not want to enter your sport...');
-        }
-    } while (trueSport(sportUser));
-    
-    switch (sportUser) {
-        case 'basketball':
-            favoriteSport = ('Cool! Do you want to be Kobe Bryant?');
-            break;
-        case 'box':
-            favoriteSport = ('Cool! Do you want to be Mike Tyson?');
-            break;
-        case 'football':
-            favoriteSport = ('Cool! Do you want to be Cristiano Ronaldo?');
-            break;
-        default:
-            otherSport = (`Cool! Do you like ${sportUser}`);
-    }
+  switch (toLowerCaseSport) {
+    case "basketball":
+      favoriteSport = "Cool! Do you want to be Kobe Bryant?";
+      break;
+    case "box":
+      favoriteSport = "Cool! Do you want to be Mike Tyson?";
+      break;
+    case "football":
+      favoriteSport = "Cool! Do you want to be Cristiano Ronaldo?";
+      break;
+    default:
+      otherSport = `Cool! Do you like ${userSport}`;
+  }
 
-    return alert(`
-        You are ${currentDate} years old;
-        ${capitalCity || otherCity};
-        ${favoriteSport || otherSport};
-        `)
+  return alert(
+        `You are ${userAge} years old;` +
+        '\n' +
+        `${capitalCity || otherCity};` +
+        '\n' +
+        `${favoriteSport || otherSport};`
+        );
 }
 
 userPortrait();
+
