90928ded7b86c5a30cf21ef5c7598ec9c137b770
R4dche <radchenko.g97@gmail.com>
2023-05-23 19:12:17 +0300

    added some cycles

---
+++ lesson-4/script.js
@@ -0,0 +1,47 @@
+// for (let i = 10; i <= 20; i++) {
+//     let result = `${i},`;
+//     console.log(result);
+// };
+let result = '';
+
+// for (let i = 10; i <= 20; i++) {
+//     result = Math.pow(i,2);
+//     console.log(result);
+// };
+
+// for (let i = 1; i <= 10; i++) {
+//     let result = `${i}` * 7;
+//     console.log(result)
+// }
+
+// for (let i = 1; i <= 15; i++) {
+//     result += i;
+// }
+
+// for (let i = 15; i <= 35; i++) {
+//     result *= i;
+// }
+
+// for (let i = 1; i <= 500; i++) {
+//     result += i;
+// }
+
+// for (let i = 30; i <= 80; i++) {
+//     if (i % 2 === 0) {
+//         result += i
+//     }
+// };
+
+
+for (let i = 100; i <= 200; i++) {
+    if (i % 3 === 0) {
+        result += `${i},`
+    }
+};
+
+console.log(result);
+
+
+
+
+
