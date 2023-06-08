fba844d5fa49637185969be9200eb849a758a942
R4dche <radchenko.g97@gmail.com>
2023-06-08 23:45:54 +0300

    fix a bug

--- lesson-5/script.js
+++ lesson-5/script.js
@@ -2,60 +2,68 @@
 let result = [];
 
 for (let i = 20; i <= 30; i = i + 0.5) {
-    result.push(i)
+  result.push(i);
 }
 
-console.log(result.join(', '));
+console.log(result.join(", "));
 
 // Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів.
 
 let dollar = 27;
 let multiDollar;
-let result1 = '';
+let result1 = "";
 
 for (let i = 10; i <= 100; i = i + 10) {
-    multiDollar = `${dollar * i}`;
-    result1 += `${i}$ = ${multiDollar} грн` + '\n';
+  multiDollar = `${dollar * i}`;
+  result1 += `${i}$ = ${multiDollar} грн` + "\n";
 }
 
 console.log(result1);
 
 // Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N.
 
-let integer = 50;
-let integerResult = [];
+function integerNumber(integer) {
+  let integerResult = [];
 
-for (let i = 1; i <= 100; i++) {
+  for (let i = 1; i <= 100; i++) {
     if (i ** 2 <= integer) {
-        integerResult.push(i);
+      integerResult.push(i);
     }
+  }
+  return console.log(integerResult.join(", "));
 }
 
-console.log(integerResult.join(', '));
+integerNumber(100);
 
 // Дане ціле число. З'ясувати, чи є воно простим (простим називається число, більше 1, які не мають інших дільників крім 1 і себе).
 
-function validateInteger (number) {
-    if (number < 2) {return false};
+function validateInteger(number) {
+  if (number < 2) {
+    return false;
+  }
 
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-        if (number % i === 0) {false};
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
     }
+  }
 
-    return true;
+  return true;
 }
 
-console.log(validateInteger(11));
+console.log(validateInteger(10));
 
-// Дане деяке число. Визначити, чи можна одержати це число шляхом зведення числа 3 у деякий ступінь. 
+// Дане деяке число. Визначити, чи можна одержати це число шляхом зведення числа 3 у деякий ступінь.
 // (Наприклад, числа 9, 81 можна отримати, а 13 - не можна).
 
-function validateSqrtThree (number) {
-    for (let i = 3; i <= number; i++) {
-        if (number % 3 === 0) {return true}
+function validateSqrtThree(number) {
+  for (let i = 3; i <= number; i++) {
+    if (number % 3 === 0) {
+      return true;
     }
+  }
 
-    return false;
-};
+  return false;
+}
 
 console.log(validateSqrtThree(81));
