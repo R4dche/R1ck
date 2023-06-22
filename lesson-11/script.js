0082f0b44d5e2248af003482f16ca7b21fd961bc
R4dche <radchenko.g97@gmail.com>
2023-06-22 23:48:56 +0300

    add function doMath

--- lesson-11/script.js
+++ lesson-11/script.js
@@ -1,15 +1,48 @@
-let randomArray = ['clear', 10, '5', 'solar', 20, 'door', 50];
+// let randomArray = ['clear', 10, '5', 'solar', 20, 'door', 50];
 
-function filter (array) {
-    let filterArray = [];
-    let avarageNumber = 0;
-    for (let i = 0; i <= array.length; i++) {
-        if (typeof array[i] === 'number') {
-            filterArray.push(array[i]);
-            avarageNumber += array[i];
-        }
+// function filter (array) {
+//     let filterArray = [];
+//     let avarageNumber = 0;
+//     for (let i = 0; i <= array.length; i++) {
+//         if (typeof array[i] === 'number') {
+//             filterArray.push(array[i]);
+//             avarageNumber += array[i];
+//         }
+//     }
+//     return avarageNumber / filterArray.length;
+// }
+
+// console.log(filter(randomArray));
+
+let mathOperation = prompt ('Please specify the mathematical action');
+let firstNumber = +prompt ('Please enter fist number');
+let secondNumber = +prompt ('Please enter second number');
+let result;
+
+function doMath (mathOperation,firstNumber,secondNumber) {
+    switch (mathOperation) {
+        case '+':
+            result = firstNumber + secondNumber;
+            break;
+        case '-':
+            result = firstNumber - secondNumber;
+            break;
+        case '*':
+            result = firstNumber * secondNumber;
+            break;
+        case '/': 
+            result = firstNumber / secondNumber;
+            break;
+        case '%':
+            result = firstNumber % secondNumber;
+            break;
+        case '^':
+            result = Math.pow(firstNumber,secondNumber);
+        default:
+            alert('Invalid operation');
+            return
     }
-    return avarageNumber / filterArray.length;
+     return result
 }
 
-console.log(filter(randomArray));
+doMath();
