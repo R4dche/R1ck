a59fad1a34fd70c1fe3eee4c47f5a796a1262896
R4dche <radchenko.g97@gmail.com>
2023-07-04 18:08:32 +0300

    Lesson 9: Function removeElement

--- lesson-9/script.js
+++ lesson-9/script.js
@@ -1,8 +1,11 @@
-let userArray = [1, 2, 3, 4, 5, 6, 7]
+const userArray = [1, 2, 3, 4, 5, 6, 7, 54, 65, 32, 12];
 
-function removeElement (array,index) {
-    let remove = array.splice(index -1, 1);
+function removeElement(array, item) {
+  const index = userArray.indexOf(item);
+  if (index !== -1) {
+    array.splice(index, 1);
+  }
 }
 
-removeElement(userArray,5);
+removeElement(userArray, 65);
 console.log(userArray);
